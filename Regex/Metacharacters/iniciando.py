37abebdb8fd41759c3f8147438b2dc8d437def87
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-04-26 15:40:31 -0300

    Criei uma subpasta

---
+++ Regex/Metacharacters/iniciando.py
@@ -0,0 +1,10 @@
+# Aprendendo a trabalhar com Regex
+# Regex é uma expressão regular
+# Pode ser usado para verificar se uma sequência contém o padrão de pesquisa especificado
+
+import re # módulo padrão para trabalhar com Regex
+
+# MetaCharacters: caracteres interpretados de maneira especial por um mecanismo Regex
+# São eles: [] . ^ $ * + ? {} () \ |
+#  [] especifica um CONJUNTO de caracteres que você deseja corresponder
+# 
--- Regex/iniciando.py
+++
@@ -1,3 +0,0 @@
-# Aprendendo a trabalhar com Regex
-# Regex é uma expressão regular
-# Pode ser usado para verificar se uma sequência contém o padrão de pesquisa especificado
