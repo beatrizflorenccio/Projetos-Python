b2f091200c2d00078382f6e3e41ed309ebc9875c
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-04-27 00:09:58 -0300

    fazendo teste

--- Regex/Metacharacters/caret.py
+++
@@ -1,7 +0,0 @@
-# Metacharacter caret ^
-# Usado para verificar se uma expressão começa com alguma coisa
-import re
-
-txt = 'instituto'
-
-x = re.search('[^i]', txt)
--- Regex/Metacharacters/periodo.py
+++
@@ -1,8 +0,0 @@
-# Metacharacter periodo .
-# Um periodo corresponde a qualquer caractere unico
-import re
-
-txt = 'maria'
-
-x = re.findall('[mar.]', txt)
-print (x)
