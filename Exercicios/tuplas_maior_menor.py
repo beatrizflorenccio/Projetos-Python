8047774ea49ccd7db70c03240eee570c41776608
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-07 19:08:18 -0300

    Create tuplas_maior_menor.py
    exercício 3 do mundo 3

---
+++ Exercicios/tuplas_maior_menor.py
@@ -0,0 +1,10 @@
+#MaBe
+
+from random import randint
+
+num = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+
+print(num, '\n')
+
+print('O menor valor é {} e o maior valor é {}'.format(min(num), max(num)))
+
