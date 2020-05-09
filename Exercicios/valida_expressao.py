cf8f40b04f0591a280afe56a1d1a58b837f63aa5
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-08 23:08:04 -0300

    Update valida_expressao.py

--- Exercicios/valida_expressao.py
+++ Exercicios/valida_expressao.py
@@ -1,2 +1,19 @@
 #MaBe
-#Exercício 083        
+#Exercício 083     
+#Validação de expressões
+
+exp = input('Digite a expressão: ')
+
+abertos = exp.count('(')
+fechados = exp.count(')')
+#print(abertos)
+#print(fechados)
+
+if abertos > fechados:
+    print('Falta de fechamento de parênteses')
+
+elif fechados > abertos:
+    print('Excesso de fechamento de parênteses')
+
+elif fechados == abertos:
+    print('Expressão válida!')    
