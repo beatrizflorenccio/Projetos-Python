1115cf9c6f757dd24b92c2b8335ea939a560a731
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-11 18:41:59 -0300

    Update matriz.py

--- Exercicios/matriz.py
+++ Exercicios/matriz.py
@@ -1,8 +1,11 @@
 #MaBe
 
-l1 = []
-l2 = []
-l3 = []
-val = [l1, l2, l3]
 
-for i in range(0, 2):
+matriz = [[0, 0, 0], [0, 0, 0,], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite o valor da posição {(c, l)}: '))
+
+print(f'{matriz[0]}')
+print(f'{matriz[1]}')
+print(f'{matriz[2]}')
