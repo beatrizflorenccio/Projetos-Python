ec33a3ad3dfebbd7c4187b910e96a80c75271cf9
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-19 23:56:45 -0300

    Create dic_nome_media.py

---
+++ Exercicios/dic_nome_media.py
@@ -0,0 +1,11 @@
+
+dados = {'Nome':input('Nome: '), 'Média':int(input('Média: '))}
+
+if dados['Média'] >= 7:
+    dados['Situação'] = 'Aprovado'
+else:
+    dados['Situação'] = 'Reprovado'
+
+print(f'Nome = {dados["Nome"]}')
+print(f'Média = {dados["Média"]}')
+print(f'Situação = {dados["Situação"]}')
