3de3ea9372e899816050f593b9839e927774845e
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-21 19:41:01 -0300

    Create func_passos.py

---
+++ Exercicios/func_passos.py
@@ -0,0 +1,33 @@
+#MaBe
+from time import sleep
+
+def conte(inicio, fim, passo):
+    print(f'Contagem de {inicio} até {fim}, de ', end='')
+    if inicio > fim:
+        if passo <= 0:
+            print(f'{-passo} em {-passo}')
+            for n in range(inicio, fim-1, passo):
+                print(n, end=' ')
+        else:
+            print(f'{passo} em {passo}')
+            for n in range(inicio, fim-1, -passo):
+                print(n, end=' ')
+    else: 
+        print(f'{passo} em {passo}')
+        for n in range(inicio, fim+1, passo):
+            print(n, end=' ')
+        sleep(1)
+    print()
+
+
+conte(1, 10, 1)
+print('-'*35)
+conte(10, 0, -2)
+
+print('-'*35)
+print('Sua vez!')
+i = int(input('>>Início da contagem: '))
+f = int(input('>>Fim da contagem: '))
+p = int(input('>>Passos: '))
+
+conte(i, f, p)
