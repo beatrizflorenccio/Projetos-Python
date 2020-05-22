c399133f73888a0eb4c564125c129902fccd6563
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-21 21:00:50 -0300

    Create func_maior.py

---
+++ Exercicios/func_maior.py
@@ -0,0 +1,16 @@
+
+
+def maior(*int):
+    print('-'*50)
+    print('Analisando...')
+    if len(int) >=0:
+        for n in int:
+            print(n, end=' ')
+        print(f'Foram recebidos {len(int)} parâmetros inteiros')
+        print(f'O maior inteiro é {max(int)}')
+    elif int :
+        print(f'Foram recebidos {len(int)} parâmetros inteiros')
+        print(f'O maior inteiro é {len(int)}')
+    print('-'*50)
+
+maior()
